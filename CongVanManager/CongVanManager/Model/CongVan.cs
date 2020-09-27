c70eab9ab9552ea16d8f34d4dec482f54dd7d64c
Long Tưởng Thành <18520227@ms.uit.edu.vn>
2020-09-27 21:35:03 +0700

    Add page switch mechanic

--- CongVanManager/CongVanManager/Model/CongVan.cs
+++ CongVanManager/CongVanManager/Model/CongVan.cs
@@ -13,48 +13,55 @@
             this.DanhSachNoiNhan = new ObservableCollection<NoiNhan>();
             this.PhanHois = new ObservableCollection<PhanHoi>();
         }
-
+        private int _id;
         public int Id
         {
             get => _id;
             set { _id = value; OnPropertyChanged(); }
         }
+        private string _soKyHieu;
         public string SoKyHieu
         {
             get => _soKyHieu;
             set { _soKyHieu = value; OnPropertyChanged(); }
         }
+        private int? _soCongVan;
         public int? SoCongVan
         {
             get => _soCongVan;
             set { _soCongVan = value; OnPropertyChanged(); }
         }
+        private DateTime? _ngayCongVan;
         public DateTime? NgayCongVan
         {
             get => _ngayCongVan;
             set { _ngayCongVan = value; OnPropertyChanged(); }
         }
+        private string _trichYeu;
         public string TrichYeu
         {
             get => _trichYeu;
             set { _trichYeu = value; OnPropertyChanged(); }
         }
+        private string _ghiChu;
         public string GhiChu
         {
             get => _ghiChu;
             set { _ghiChu = value; OnPropertyChanged(); }
         }
+        private string _pDFScanLocation;
         public string PDFScanLocation
         {
             get => _pDFScanLocation;
             set { _pDFScanLocation = value; OnPropertyChanged(); }
         }
+        private DateTime _ngayXuLi;
         public DateTime NgayXuLi
         {
             get => _ngayXuLi;
             set { _ngayXuLi = value; OnPropertyChanged(); }
         }
-
+        private int _statusCode;
         public int StatusCode
         {
             get => _statusCode;
@@ -74,17 +81,19 @@
                 else return "Chưa đọc";
             }
         }
-
+        private LoaiCongVan _loaiCongVan;
         public virtual LoaiCongVan LoaiCongVan
         {
             get => _loaiCongVan;
             set { _loaiCongVan = value; OnPropertyChanged(); }
         }
+        private LienHe _noiGui;
         public virtual LienHe NoiGui
         {
             get => _noiGui;
             set { _noiGui = value; OnPropertyChanged(); }
         }
+        private ObservableCollection<NoiNhan> _danhSachNoiNhan;
         public virtual ObservableCollection<NoiNhan> DanhSachNoiNhan { get => _danhSachNoiNhan;
             set {
                 value.CollectionChanged +=
@@ -101,28 +110,20 @@
         }
 
         private static ObservableCollection<CongVan> _db;
-        private int _id;
-        private string _soKyHieu;
-        private int? _soCongVan;
-        private DateTime? _ngayCongVan;
-        private string _trichYeu;
-        private string _ghiChu;
-        private string _pDFScanLocation;
-        private DateTime _ngayXuLi;
-        private int _statusCode;
-        private LoaiCongVan _loaiCongVan;
-        private LienHe _noiGui;
-        private ObservableCollection<NoiNhan> _danhSachNoiNhan;
-
+ 
         public static ObservableCollection<CongVan> DB
         {
             get
             {
                 if (_db == null)
+                {
                     _db = new ObservableCollection<CongVan>();
+                    // TODO: syncronize mechanic
+                }
                 return _db;
             }
             private set { }
         }
+        // TODO: syncronize mechanic
     }
 }
---
+++ CongVanManager/CongVanManager/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CongVanManager.ViewModel
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private readonly Page[] page = { new InboxLayout() };
+        private Page _selectedPage;
+        public Page SelectedPage
+        {
+            get {
+                return _selectedPage;
+            }
+            set {
+                _selectedPage = value; OnPropertyChanged();
+            }
+        }
+        public MainWindowViewModel()
+        {
+            SelectedPage = page[0];
+        }
+
+        public void ChangePage(int pageNumber)
+        {
+            SelectedPage = page[pageNumber];
+        }
+
+        public void PageSwitch(int messageID)
+        {
+            switch (messageID)
+            {
+                case 0:
+                    ChangePage(0);
+                    break;
+                case 1:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
