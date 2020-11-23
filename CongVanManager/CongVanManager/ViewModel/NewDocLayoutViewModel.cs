c5aee39e0ed4f3b19fd20eb7173c5f1080ee567d
UIT-SE-TrungVo <66295737+UIT-SE-TrungVo@users.noreply.github.com>
2020-11-23 13:42:44 +0700

    Update Sender UX

--- CongVanManager/CongVanManager/ViewModel/NewDocLayoutViewModel.cs
+++ CongVanManager/CongVanManager/ViewModel/NewDocLayoutViewModel.cs
@@ -1,10 +1,12 @@
-﻿using CongVanManager.View;
+﻿using CongVanManager.Command;
+using CongVanManager.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Documents;
+using System.Windows.Input;
 
 public enum DocType { In, Out };
 
