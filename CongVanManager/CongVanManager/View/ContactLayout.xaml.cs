9866388be08613bb88ff95351c80aba0d4bec9bf
Luong Ly Cong Thang <57753987+thangluong2202@users.noreply.github.com>
2020-12-04 12:53:35 +0700

    create Contact VM

--- CongVanManager/CongVanManager/View/ContactLayout.xaml.cs
+++ CongVanManager/CongVanManager/View/ContactLayout.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CongVanManager.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace CongVanManager.View
         public ContactLayout()
         {
             InitializeComponent();
+            this.DataContext = new ContactLayoutViewModel();
         }
     }
 }
---
+++ CongVanManager/CongVanManager/ViewModel/ContactLayoutViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CongVanManager.Command;
+using CongVanManager.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace CongVanManager.ViewModel
+{
+    public class ContactLayoutViewModel : ObservableObject
+    {
+        public ContactLayoutViewModel()
+        {
+
+        }
+    }
+}
